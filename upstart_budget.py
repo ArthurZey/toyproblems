3758d5b13024ff246b41ade6ad24ef6de4014d86
Arthur Zey <Arthur@DeltaWerx.com>
2015-08-19 12:36:08 -0700

    Cleaned up testing suite; tightened up and documented code in preparation for implementing goals

--- upstart_budget.py
+++ upstart_budget.py
@@ -45,59 +45,88 @@ from statistics import mean
 
 class Budget:
   def __init__(self, args):
-    self.args        = args
-    self.parsed_args = self.parse_args()
-    self.incomes     = self.parse_income()
-    self.goals       = self.parse_goals()
+    self.parsed_args = self.parse_args(args)
+    self.incomes     = self.parse_incomes(self.parsed_args.incomes)
+    self.goals       = self.parse_goals(self.parsed_args.goals)
 
 
-  def parse_args(self):
+  def parse_args(self, args):
+    '''Parse arguments into variables readily available to the class for further processing; provides built-in niceties for calling the script from the command line.'''
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("income", help="a quotation-enclosed, comma-separated list of expected net income (whole dollars) for each of the next N years;\nfor example: \"85000,110000,120000,131000,133000,155000,160000,295000,295000,350000\"\nNote that non-numeric characters will be stripped, so \"34k8\" will be interpreted as \"348\".", type=str)
+    parser.add_argument("incomes", help="a quotation-enclosed, comma-separated list of expected net income (whole dollars) for each of the next N years;\nfor example: \"85000,110000,120000,131000,133000,155000,160000,295000,295000,350000\"\nNote that non-numeric characters will be stripped, so \"34k8\" will be interpreted as \"348\".", type=str)
     parser.add_argument("goals", help="a quotation-enclosed, comma-separated list of year-to-goal associations (where years are 1-indexed and not beyond the years specified in the \"income\" parameter);\nfor example: \"1:500,4:8472,4:1701,10:100000\"\nNote that non-numeric characters will be stripped, so \"34k8\" will be interpreted as \"348\".", type=str)
-    return parser.parse_args(self.args)
+    return parser.parse_args(args)
 
-  def parse_income(self):
-    return [float(x.strip()) for x in "".join([c for c in self.parsed_args.income if c in "0123456789,"]).split(",")]
+  def parse_incomes(self, incomes_string):
+    '''Parse (and sanitize) a string of year-by-year incomes into a list of floating-point values.'''
 
-  def parse_goals(self):
-    result = dict()
-    goals_list = [x.strip() for x in "".join([c for c in self.parsed_args.goals if c in "0123456789,:"]).split(",")]
-    for x in goals_list:
-      year_goal = [float(y.strip()) for y in x.split(":")]
+    return [float(x.strip()) for x in "".join([c for c in incomes_string if c in "0123456789,"]).split(",")]
+
+  def parse_goals(self, goals_string):
+    '''Parse (and sanitize) a string of year:dollar goals into a list of year-by-year goals, with multiple goals for a given year combined.
+
+      Presupposes that incomes have already been parsed into self.incomes.'''
+
+    # Create an empty list for each year: If this is being called within an actual class instantiation, use len(self.incomes). Otherwise, use the number of colons in the input string as an upper bound.
+    result = [0 for x in [c for c in goals_string if c == ":"]] if self is None else [0 for x in self.incomes]
+    for year_to_goal_string in [x.strip() for x in "".join([c for c in goals_string if c in "0123456789,:"]).split(",")]:
+      result[int(year_to_goal_string.split(":")[0].strip())-1] += float(year_to_goal_string.split(":")[1].strip())
+
+    '''
+    for goal in goals_list:
+      year_goal = [float(amount.strip()) for amount in goal.split(":")]
       if year_goal[0] in result:
         result[year_goal[0]] += year_goal[1]
       else:
         result[year_goal[0]] = year_goal[1]
+    '''
     return result
 
-  def compute_budget(self):
-    budget = self.incomes
-    for i in range(len(budget) - 1, 0, -1):
-      average = mean(budget[:(i+1)])
-      if budget[i] < average:
-        j = i - 1
-        while budget[i] < average and j >= 0:
-          if budget[j] > average:
-            delta_j = budget[j] - average
-            if delta_j > average - budget[i]:
-              budget[j] -= average - budget[i]
-              budget[i] = average
-            else:
-              budget[i] += delta_j
-              budget[j] = average
-          j -= 1
-      elif i < len(budget) - 1 and budget[i] > budget[i+1]:
-        # need to find the next index that budget[i] is smaller than
-        j = i + 1
-        while j < len(budget) and budget[i] > budget[j]:
+  def distribute_income(self, incomes):
+    '''Take in list of incomes for consecutive years, and return a distribution of the optimal spending over those years, assuming no particular spending requirements/goals in any year. Optimal spending means minimizing the decreasing marginal utility of each additional dollar spent. I believe this can also be thought of as minimizing the standard deviation.
+
+    General strategy:
+    0. Key observation that's relevant to the consideration of the mathematics and algorithm design: The optimal spending will be monotonically increasing, year-over-year.
+    1. Start at the last year (and move backward).
+    2. For any given year, calculate the average of all the years leading up to and including it.
+      a. If the the income in that year is below that average, reallocate income from the nearest previous year that's above the average. (Mathematically, it's equivalent to allocate everything from the immediately previous year, even though that might put it under the average or even negative!)
+      b. If the income is above the average, but less than the following year (the previously _considered_ year in our algorithm), do nothing.
+      c. If the income is above the following year, take the average of those two years and distribute evenly among them. Continue considering the next following year, and if the new average is bigger than that next year, average across all three and distribute evenly. Continue until the average is no longer greater than the next year considered.
+    3. Move on to the previous year.
+    '''
+
+    # Initialize the distribution with the current given incomes.
+    distribution = [float(x) for x in incomes]
+
+    # Start at the end and move backward.
+    for year_index in range(len(distribution) - 1, 0, -1):
+      # Compute the average income of all the years leading up to the year under consideration.
+      average = mean(distribution[:(year_index + 1)])
+
+
+      if distribution[year_index] < average:
+        # Borrow from the previous year the amount necessary to get this year to the average.
+        distribution[year_index - 1] -= average - distribution[year_index]
+
+        # Allocate that amount to the current year by just setting it to the average.
+        distribution[year_index] = average
+
+      elif year_index < len(distribution) - 1 and distribution[year_index] > distribution[year_index + 1]:
+        # We need to keep track of how many years we can distribute over, and we'll be considering them one by one. We should be treating all consecutive years together (since that's how we might reason about it on paper), but because of how averaging works, we'll end up with a mathematically equivalent result.
+        j = year_index + 1
+
+        # Look over (and potentially distribute to) all the following years where this year's distribution (which is modified "in place") is greater than that year.
+        while j < len(distribution) and distribution[year_index] > distribution[j]:
+          # Replace all the values between year_index and j with the average of all the values between year_index and j.
+          average = mean(distribution[year_index:j+1])
+          for k in range(year_index, j+1):
+            distribution[k] = average
+
+          # Move on to the next year that might need to be distributed to.
           j += 1
-        average = mean(budget[i:j])
-        for k in range(i, j):
-          budget[k] = average
 
-    return [round(x, 10) for x in budget]
-
+    return [round(x, 10) for x in distribution]
 
 def main():
   the_budget = Budget(sys.argv[1:])
--- upstart_budget.test.py
+++ upstart_budget.test.py
@@ -25,91 +25,45 @@ class BudgetTestCases(unittest.TestCase):
   def tearDown(self):
     self.budgets = None
 
-  '''
   def test_parse_args(self):
-    self.assertEqual(self.budget.parse_args().income, "80,40,70,200,10,70,0,100,90,10")
-    self.assertEqual(self.budget.parse_args().goals, "1:0")
+    inputs_to_outputs = {
+      ("80,40,70,200,10,70,0,100,90,10", "1:0"): ["80,40,70,200,10,70,0,100,90,10", "1:0"],
+    }
+    for (input_args, output_args) in inputs_to_outputs.items():
+      self.assertEqual(Budget.parse_args(None, input_args).incomes, output_args[0])
+      self.assertEqual(Budget.parse_args(None, input_args).goals, output_args[1])
 
-  def test_parse_income(self):
-    self.assertEqual(self.budget.parse_income(), [80,40,70,200,10,70,0,100,90,10])
-
+  def test_parse_incomes(self):
+    inputs_to_outputs = {
+      "80,40,70,200,10,70,0,100,90,10": [80,40,70,200,10,70,0,100,90,10],
+    }
+    for (input_string, output_list) in inputs_to_outputs.items():
+      self.assertEqual(Budget.parse_incomes(None, input_string), output_list)
+  
   def test_parse_goals(self):
-    self.assertEqual(self.budget.parse_goals(), {1:0})
-  '''
-  def test_compute_budget(self):
-    solutions = [
-      [60,60,68.75,68.75,68.75,68.75,68.75,68.75,68.75,68.75],
-      [500/3, 500/3, 500/3, 500],
-      [550,550,1450,1450],
-      [500,500,500,500],
-      [0,0,1000,1000],
-      [100,150,150,500],
-      [1004/3,1004/3,1004/3,1004],
-      [251,251,251,251],
-      [1,1,1,1001],
-    ]
-    for (i, budget) in enumerate(self.budgets):
-      self.assertEqual(budget.compute_budget(), [round(float(x), 10) for x in solutions[i]])
-
-
-class ComplexBudgetTestCase(unittest.TestCase):
-  def setUp(self):
-    #self.budget = Budget(["500,500,500", "3:900"])
-    pass
-
-  def tearDown(self):
-    #self.budget = None
-    pass
-
-  def test_parse_args(self):
-    #self.assertEqual(self.budget.parse_args().income, "500,500,500")
-    #self.assertEqual(self.budget.parse_args().goals, "3:900")
-    pass
-    '''
-    args = Budget.parse_args(["some kind of income string", "some kind of goals string"])
-    self.assertEqual(args.income, "some kind of income string")
-    self.assertEqual(args.goals, "some kind of goals string")
-    self.assertEqual(args.tolerance, None)
-
-    args = Budget.parse_args(["-t", "1234", "income", "goals"])
-    self.assertEqual(args.income, "income")
-    self.assertEqual(args.goals, "goals")
-    self.assertEqual(args.tolerance, 1234)
-    '''
-
-  def test_parse_income(self):
-    #self.assertEqual(self.budget.parse_income(), [500, 500, 500])
-    pass
-    '''
-    self.assertEqual(
-      Budget.parse_income("85000,110000,120000,131000,133000,155000,160000,295000,295000,350000"),
-      [85000,110000,120000,131000,133000,155000,160000,295000,295000,350000]
-      )
-
-    # more tests of variously formated inputs?
-    '''
-
-  def test_parse_goals(self):
-    #self.assertEqual(self.budget.parse_goals(),{3:900})
-    pass
-    '''
-    self.assertEqual(
-      Budget.parse_goals("1:500,4:8472,4:1701,10:100000"),
-      {1:500, 4:10173, 10:100000}
-      )
-
-    # more tests of variously formated inputs?
-    '''
-
-  def test_compute_budget(self):
-    #self.assertEqual(self.budget.compute_budget(), {1:200, 2:200, 3:200})
-    pass
-    '''
-    self.assertEqual(
-      Budget.create_budget([500, 500, 500], {3:900}, 0),
-      {1:200, 2:200, 3:200}
-      )
-    '''
+    inputs_to_outputs = {
+      "1:0": [0],
+    }
+    for (input_string, output_list) in inputs_to_outputs.items():
+      self.assertEqual(Budget.parse_goals(None, input_string), output_list)
+  
+  def test_distribute_income(self):
+    inputs_to_outputs = {
+      (80,40,70,200,10,70,0,100,90,10):[60,60,68.75,68.75,68.75,68.75,68.75,68.75,68.75,68.75],
+      (500,0,0,500):[500/3, 500/3, 500/3, 500],
+      (1000,100,2000,900):[550,550,1450,1450],
+      (1000,1000,0,0):[500,500,500,500],
+      (0,0,1000,1000):[0,0,1000,1000],
+      (100,300,0,500):[100,150,150,500],
+      (1001,0,3,1004):[1004/3,1004/3,1004/3,1004],
+      (1001,0,3,0):[251,251,251,251],
+      (3,0,0,1001):[1,1,1,1001],
+      (10,8,8,9,9):[26/3,26/3,26/3,9,9],
+      (10,5,5,6,6):[6.4,6.4,6.4,6.4,6.4],
+      (10,4,4,9,9):[6,6,6,9,9],
+    }
+    for (input_incomes, output_distribution) in inputs_to_outputs.items():
+      self.assertEqual(Budget.distribute_income(None, input_incomes), [round(float(x), 10) for x in output_distribution])
 
 
 if __name__ == '__main__':
