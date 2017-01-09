76e0d336b9bbcfa74a5f3aa16fb17b2ad5c4f8f3
Arthur Zey <arthur.zey@autodesk.com>
2017-01-08 17:57:05 -0800

    stormpath: add callback handling

---
+++ interviews/stormpath/callback.py
@@ -0,0 +1,22 @@
+#!/home/arthurzey/deltawerx.com/env/bin/python
+
+from os.path import expanduser, join
+
+from stormpath.client import Client
+
+
+# This is the absolute path to the file ~/.stormpath/apiKey.properties
+# downloaded in the previous section.  This will work on any OS.
+API_KEY_FILE = join(expanduser('~'), '.stormpath', 'apiKey.properties')
+
+
+client = Client(api_key_file=API_KEY_FILE)
+
+application = client.applications.search({'name': 'My Application'})[0]
+
+result = application.handle_stormpath_callback(url)
+
+print "Location: http://www.deltawerx.com/toyproblems/interviews/stormpath/"
+print "Content-type: text/html\n\n"
+
+print "You are being redirected to http://www.deltawerx.com/toyproblems/interviews/stormpath/"
