35036bae863c3893760d7868ad9442ca6bac515d
Arthur Zey <arthur.zey@autodesk.com>
2017-01-08 16:56:14 -0800

    stormpath: removed quickstart; created login link for ID Site

--- interviews/stormpath/index.py
+++ interviews/stormpath/index.py
@@ -1,26 +1,4 @@
 #!/home/arthurzey/deltawerx.com/env/bin/python
 print "Content-type: text/html\n\n"
 
-from os.path import expanduser, join
-
-from stormpath.client import Client
-
-
-# This is the absolute path to the file ~/.stormpath/apiKey.properties
-# downloaded in the previous section.  This will work on any OS.
-API_KEY_FILE = join(expanduser('~'), '.stormpath', 'apiKey.properties')
-
-
-client = Client(api_key_file=API_KEY_FILE)
-
-application = client.applications.search({'name': 'My Application'})[0]
-
-from stormpath.error import Error as StormpathError
-
-try:
-    result = application.authenticate_account('tk421@galacticempire.co', 'Changeme123!')
-    account = result.account
-
-    print('User ' + result.account.full_name + ' logged in.')
-except StormpathError, e:
-    print('Could not log in. Error: ' + e.user_message)
+print('<a href="./login">Login</a>')
