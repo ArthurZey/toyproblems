dde2d89167ed8910155cf0d495ab5b405a5690e6
Arthur Zey <arthur.zey@autodesk.com>
2017-01-08 16:14:01 -0800

    stormpath: initializing the client

--- interviews/stormpath/index.py
+++ interviews/stormpath/index.py
@@ -1,3 +1,16 @@
 #!/home/arthurzey/deltawerx.com/env/bin/python
 print "Content-type: text/html\n\n"
+
+from os.path import expanduser, join
+
+from stormpath.client import Client
+
+
+# This is the absolute path to the file ~/.stormpath/apiKey.properties
+# downloaded in the previous section.  This will work on any OS.
+API_KEY_FILE = join(expanduser('~'), '.stormpath', 'apiKey.properties')
+
+
+client = Client(api_key_file=API_KEY_FILE)
+
 print "Hello!"
